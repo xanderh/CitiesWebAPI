7a232ddf5037976bd5312e6ebcf1d79542bc2606
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-11-05 11:23:07 +0100

    Set up basic HTTP actions

--- CitiesWebAPI/Controllers/CitiesController.cs
+++ CitiesWebAPI/Controllers/CitiesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CitiesWebAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,68 @@ namespace CitiesWebAPI.Controllers
     [ApiController]
     public class CitiesController : ControllerBase
     {
+        private List<City> _cities = new List<City>()
+            {
+                new City(){Id=1,Name="Odense", Description="Brånner!", PointsOfInterest=new List<PointOfInterest>{ new PointOfInterest(){ Id = 1, Name = "Brandts", Description = "Kunst!" }, new PointOfInterest(){ Id = 2, Name = "H. C. Andersens hus", Description = "Kultur!" } } },
+                new City(){Id=2,Name="Aalborg", Description="prlør!", PointsOfInterest= new List<PointOfInterest>{ new PointOfInterest(){ Id=3, Name="Vandtårnet", Description="Der er fucking meget vand"}, new PointOfInterest(){ Id = 4, Name = "Friluftsbadet", Description = "Der er endnu mere vand" } } }
+            };
+        public CitiesController()
+        {
+
+        }
+
+        [Route("Cities")]
+        public IActionResult GetCities(bool ShouldGetPointsOfInterest = false)
+        {
+            List<City> cities = _cities;
+            if (!ShouldGetPointsOfInterest)
+            {
+                return new ObjectResult(cities.Select(x => new { x.Id, x.Name, x.Description }));
+            }
+            return new ObjectResult(cities);
+        }
+
+        [Route("City/{id}")]
+        public IActionResult GetCity(int id, bool ShouldGetPointsOfInterest = false)
+        {
+            if (!_cities.Exists(x => x.Id == id))
+            {
+                return NotFound();
+            }
+            if (!ShouldGetPointsOfInterest)
+            {
+                return new ObjectResult(_cities.FindAll(x => x.Id == id).Select(x => new { x.Id, x.Name, x.Description }));
+            }
+            return new ObjectResult(_cities.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public IActionResult CreateCity(City city)
+        {
+            _cities.Add(city);
+            return CreatedAtAction("CreateCity", city);
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public IActionResult DeleteCity(int id)
+        {
+            _cities.RemoveAll(x => x.Id == id);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public IActionResult Update(City city)
+        {
+            if(!_cities.Exists(x => x.Id == city.Id))
+            {
+                return NotFound();
+            }
+            City oldCity = _cities.FirstOrDefault(x => x.Id == city.Id);
+            oldCity = city;
+            return Ok();
+        }
     }
 }
--- CitiesWebAPI/Controllers/PointsOfInterestController.cs
+++ CitiesWebAPI/Controllers/PointsOfInterestController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CitiesWebAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,59 @@ namespace CitiesWebAPI.Controllers
     [ApiController]
     public class PointsOfInterestController : ControllerBase
     {
+        private List<City> _cities = new List<City>()
+            {
+                new City(){Id=1,Name="Odense", Description="Brånner!", PointsOfInterest=new List<PointOfInterest>{ new PointOfInterest(){ Id = 1, Name = "Brandts", Description = "Kunst!" }, new PointOfInterest(){ Id = 2, Name = "H. C. Andersens hus", Description = "Kultur!" } } },
+                new City(){Id=2,Name="Aalborg", Description="prlør!", PointsOfInterest= new List<PointOfInterest>{ new PointOfInterest(){ Id=3, Name="Vandtårnet", Description="Der er fucking meget vand"}, new PointOfInterest(){ Id = 4, Name = "Friluftsbadet", Description = "Der er endnu mere vand" } } }
+            };
+
+        [Route("PointsOfInterest/{id}")]
+        public IActionResult GetPointsOfInterest(int id)
+        {
+            if(!_cities.Exists(x=> x.Id == id)) {
+                return NotFound();
+            }
+            return new OkObjectResult(_cities.FirstOrDefault(x => x.Id == id).PointsOfInterest);
+        }
+
+        [Route("PointOfInterest/{cityId}/{poiId}")]
+        public IActionResult GetPointOfInterest(int cityId, int poiId)
+        {
+            if (!_cities.Exists(x => x.Id == cityId) || !_cities.FirstOrDefault(x => x.Id == cityId).PointsOfInterest.Exists(x => x.Id == poiId))
+            {
+                return NotFound();
+            }
+            return new OkObjectResult(_cities.FirstOrDefault(x => x.Id == cityId).PointsOfInterest.FirstOrDefault(x => x.Id == poiId));
+        }
+
+        [HttpPost]
+        [Route("Create/{id}")]
+        public IActionResult CreatePoi(int id, PointOfInterest poi)
+        {
+            _cities.FirstOrDefault(x => x.Id == id).PointsOfInterest.Add(poi);
+            return CreatedAtAction("CreatePoi", poi);
+        }
+
+        [HttpDelete]
+        [Route("Delete/{cityId}/{poiId}")]
+        public IActionResult DeleteCity(int cityId, int poiId)
+        {
+            _cities.FirstOrDefault(x => x.Id == cityId).PointsOfInterest.RemoveAll(x => x.Id == poiId);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("Update/{id}")]
+        public IActionResult Update(int id, PointOfInterest poi)
+        {
+            if (!_cities.Exists(x => x.Id == id))
+            {
+                return NotFound();
+            }
+            City city = _cities.FirstOrDefault(x => x.Id == id);
+            PointOfInterest oldPoi = city.PointsOfInterest.FirstOrDefault(x => x.Id == poi.Id);
+            oldPoi = poi;
+            return Ok();
+        }
     }
 }
