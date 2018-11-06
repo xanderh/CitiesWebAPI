defd6d856d32fd4a45d889da2bab3e1e10cd8bd5
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-11-06 10:42:11 +0100

    Added CityId for Foreign Key relation

--- CitiesWebAPI/Models/PointOfInterest.cs
+++ CitiesWebAPI/Models/PointOfInterest.cs
@@ -10,5 +10,6 @@ namespace CitiesWebAPI.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int CityId { get; set; }
     }
 }
