6fb26fc121f2986d425e99fedea93c0ec18c4a33
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-11-06 10:41:51 +0100

    Implemented EF DbContext

--- CitiesWebAPI/Models/DataContext.cs
+++ CitiesWebAPI/Models/DataContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CitiesWebAPI.Models
 {
-    public class DataContext 
+    public class DataContext : DbContext
     {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; }
+
+        public DataContext(DbContextOptions options): base(options)
+        {
+            Database.EnsureCreated();
+        }
     }
 }
