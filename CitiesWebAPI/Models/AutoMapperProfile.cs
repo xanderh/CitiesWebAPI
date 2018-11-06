488b786c9602d3dfd16d2bd81fb9234e47cde7bf
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-11-06 10:41:10 +0100

    Added automapper profile

---
+++ CitiesWebAPI/Models/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CitiesWebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CitiesWebAPI.Models
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<City, CityDTO>();
+            CreateMap<CityDTO, City>();
+            CreateMap<City, CityDetailedDTO>();
+            CreateMap<CityDetailedDTO, City>();
+        }
+    }
+}
