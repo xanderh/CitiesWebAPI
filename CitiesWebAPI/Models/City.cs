d39e2826baab861f0167ac1523b1b947013b4273
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-11-13 12:08:25 +0100

    Added concurrency handling

--- CitiesWebAPI/Models/City.cs
+++ CitiesWebAPI/Models/City.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace CitiesWebAPI.Models
     public class City
     {
         public int Id { get; set; }
+        [ConcurrencyCheck]
         public string Name { get; set; }
+        [ConcurrencyCheck]
         public string Description { get; set; }
+
 
         public List<PointOfInterest> PointOfInterests { get; set; }
     }
