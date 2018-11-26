983b7f413e374938a1c8d6c38e0f23fca682221b
Alexander Hvilshøj <alexander@hvilshoej.dk>
2018-11-26 15:51:55 +0100

    Added validateModel data annotation

---
+++ CitiesWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using CitiesWebAPI.Filters;
+
+namespace CitiesWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [ValidateModel]
+        [HttpPost("token")]
+        public IActionResult CreateToken()
+        {
+            
+        }
+    }
+}
